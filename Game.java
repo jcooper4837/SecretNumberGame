72f6cb0252fa6b4609204b727708451c301e4bc1
Cooper Martin <jcooper4837@gmail.com>
2022-07-20 16:12:50 -0600

    First commit

---
+++ Game.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Cooper Martin
+ * @version Secret Number Game
+ * @since 7/20/2022
+ */
+
+public class Game
+{
+    public static void RunGame()
+    {
+        
+    }
+    public static void main(String[] args)
+    {
+        
+    }
+}
